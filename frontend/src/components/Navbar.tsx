ea1f0903eb43937df52fc08266c337f70d9d73c9
Hargun <hargunsibal@gmail.com>
2023-06-27 12:53:26 -0400

    Home page styling a bit done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className='flex items-center justify-between px-4 py-2 bg-blue-800 text-white'>
+    <nav className='flex items-center justify-between px-4 py-2 bg-black text-white'>
       <div className='text-xl font-bold'>Melody Match</div>
     </nav>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { redirectToAuthCodeFlow, getAccessToken } from "../components/Login";
 const Home = () => {
   const params = new URLSearchParams(window.location.search);
   const [code, setCode] = useState(params.get("code"));
-  
+
   const HandleLogin = () => {
     if (!code) {
       redirectToAuthCodeFlow();
@@ -21,20 +21,21 @@ const Home = () => {
     <>
       <NavBar />
       <div className="content">
-        <div className="left-element"></div>
-        <div className="right-element absolute right-0 w-1/3 h-full bg-gray-200 flex flex-col items-center justify-center">
-          <p className="mx-12 my-3 text-center text-3xl font-bold">
-            Discover your perfect soundtrack. Scroll your way to music bliss with Melody Match.{" "}
+        <div className="left-element absolute left-0 w-2/3 h-full bg-black flex flex-col items-center justify-center bg-black"></div>
+        <div className="right-element absolute right-0 w-1/3 h-full bg-black flex flex-col items-center justify-center">
+          <p className="mx-12 my-3 text-center text-3xl text-white font-bold">
+            Discover your perfect soundtrack. Scroll your way to music bliss with
+            <span className="text-spotifyGreen"> Melody Match.</span>{" "}
           </p>
-          <p className="mx-12 my-3 text-center">
-            Personalized recommendations await as you embark on a journey to find your next favorite song. Log in NOW
+          <p className="mx-12 my-3 text-center text-white">
+            Personalized recommendations await as you embark on a journey to find your next favorite song. Log in now
             and unlock a world of musical possibilities.
           </p>
           <Button
             onClick={HandleLogin}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="max-w-sm bg-white text-spotifyBlack font-bold py-2 px-4 rounded-xl hover:bg-slate-200"
           >
-            Log in to Spotify
+            Log in to <span className="text-spotifyGreen">Spotify</span>
           </Button>
         </div>
       </div>
