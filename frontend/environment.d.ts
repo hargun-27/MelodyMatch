08125e7dccd82f4fe52ab9bcd40d4e778aa3084f
Hargun <hargunsibal@gmail.com>
2023-06-27 13:22:03 -0400

    env file added and gitignore updated

---
+++ frontend/environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            SPOTIFY_CLIENT_ID: string;
+            SPOTIFY_CLIENT_SECRET: string;
+            NODE_ENV: 'development' | 'production';
+        }
+    }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export { }
