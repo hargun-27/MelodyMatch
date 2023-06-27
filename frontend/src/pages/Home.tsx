af063bb09653a5fa9c09def9bc18a0ad9001addb
Alvin787 <alvinhalo4@gmail.com>
2023-06-26 23:05:33 -0400

    Flex box on the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,9 +15,10 @@ const Home = () => {
       <div className="left-element">
 
       </div>
-      <div className="right-element">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae tortor id justo fringilla fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris non nunc vitae enim tincidunt lacinia. Fusce in fringilla sapien, sit amet varius sapien.</p>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Log in to Spotify</button>
+      <div className="right-element absolute right-0 w-1/3 h-full bg-gray-200 flex flex-col items-center justify-center">
+        <p className="p-8 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae tortor id justo fringilla fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris non nunc vitae enim tincidunt lacinia. Fusce in fringilla sapien, sit amet varius sapien.</p>
+        
+        <Button onClick={handleLogIn} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Log in to Spotify</Button>
       </div>
     </div>
       
