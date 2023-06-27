d97c01536487d603e210017a656cc27b7454e46f
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2023-06-26 21:10:39 -0400

    add home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,1 +1,15 @@
-export {};
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Discover your perfect soundtrack. Scroll your way to music bliss with MusicMatch</h1>
+      <p>
+        Personalized recommendations await as you embark on a journey to find your next favorite song. Log in NOW and unlock a world of musical possibilities.
+      </p>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Log in to Spotify</button>
+    </>
+  );
+};
+
+export default Home;
