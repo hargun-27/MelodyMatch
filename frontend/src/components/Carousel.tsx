b04925bf667950150d500f8289e4665cd85182b7
Hargun <hargunsibal@gmail.com>
2023-06-27 22:47:00 -0400

    Home page updated and fixed

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -22,27 +22,27 @@ const Carousel: React.FC<CarouselProps> = ({ songs }) => {
       <AnimatePresence initial={false} custom={currentIndex}>
         {songs.map((song, index) => {
           const position = (index - currentIndex + songs.length) % songs.length;
-          const x = position * -40; // Adjust this value for desired spacing
-
+          const x = (position - Math.floor(songs.length / 2)) * -(30 + 40);
+          
           // Calculate opacity based on position
           const opacity = position === 0 ? 1 : 0.5;
-
+          
           // Calculate scale based on position
-          const scale = position === 0 ? 1 : 0.9;
-
+          const scale = 1 - 0.1 * Math.abs(position);
+          
           // Calculate z-index based on position
-          const zIndex = position === 0 ? 1 : 0;
-
+          const zIndex = songs.length - position;
+          
           return (
             <motion.div
-                key={index}
-                className="absolute top-0 left-0"
-                initial={{ x: 40, opacity, scale }} // Initial animation properties when the song enters
-                animate={{ x, opacity: 1, scale }} // Animation properties during the presence of the song
-                exit={{ x: -40, opacity, scale }} // Animation properties when the song exits
-                transition={{ duration: 0.5 }} // Animation duration
-                custom={currentIndex} // Custom prop to track changes in the currentIndex state
-                style={{ zIndex }} // Apply calculated z-index to control stacking order
+              key={index}
+              className="absolute top-0 left-0"
+              initial={{ x: -60, opacity, scale }} // Initial animation properties when the song enters
+              animate={{ x, opacity: 1, scale }} // Animation properties during the presence of the song
+              exit={{ x: 60, opacity, scale }} // Animation properties when the song exits
+              transition={{ duration: 0.5 }} // Animation duration
+              custom={currentIndex} // Custom prop to track changes in the currentIndex state
+              style={{ zIndex }} // Apply calculated z-index to control stacking order
             >
               <Card song={song} />
             </motion.div>
